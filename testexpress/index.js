dcf99fb5de47933c054423375dbea9a112e798f0
Colin WANG <yuanrenc@gmail.com>
2021-08-25 10:04:39 +1000

    lesson 1

--- testexpress/index.js
+++ testexpress/index.js
@@ -19,59 +19,46 @@ app.post("/tasks", (req, res) => {
 });
 
 app.get("/tasks", (req, res) => {
-  res.status(200).json(data);
+  return res.status(200).json(data);
 });
 
 app.get("/tasks/:id", (req, res) => {
-  const { id } = req.params;
-  const idData = [];
-  for (var i = 0; i < data.length; i++) {
-    if (data[i].id == id) {
-      idData.push(data[i]);
-      return;
-    }
-  }
-  console.log(idData);
-  if (idData.length > 0) {
-    res.status(200).json(idData);
-  } else {
-    res.status(404).send("Task not found");
-  }
+  res.json();
 });
+//   console.log(idData);
+//   if (idData.length > 0) {
+//     res.status(200).json(idData);
+//   } else {
+//     res.status(404).send("Task not found");
+//   }
+// });
 
 app.put("/tasks/:id", (req, res) => {
-  const { id, description, done } = req.body;
-  if (req.params.id != id) {
-    res.status(404).send("Task id did not match.");
-  } else {
-    const targettask = [];
-    for (const task of data) {
-      if (task.id == id) {
-        task.description = description;
-        task.done = done;
-        targettask.push(task);
-        res.status(200).send("The task successfully updated");
-      }
+  const { description, done } = req.body;
+  const targettask = [];
+  for (const task of data) {
+    if (task.id == id) {
+      task.description = description;
+      task.done = true;
+      targettask.push(task);
+      return res.status(200).send("The task successfully updated");
     }
-    if (targettask.length == 0) {
-      res.status(404).send("Task did not found.");
-    }
+  }
+  if (targettask.length == 0) {
+    return res.status(404).send("Task did not found.");
   }
 });
 
 app.delete("/tasks/:id", (req, res) => {
   const { id } = req.params;
-  if (!ids.has(id)) {
-    res.status(404).send("Tasks not found.");
-  } else {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id == id) {
-        data.splice(i, 1);
-        console.log(data);
-        res.status(204).send("The task successfully deleted");
-      }
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      data.splice(i, 1);
+      console.log(data);
+      return res.status(204).send("The task successfully deleted");
     }
   }
+  return res.status(404).send("Tasks not found.");
 });
 
 function cors(req, res, next) {
